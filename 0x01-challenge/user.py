22d67e617effee1146476154b7fa1072274bb787
INNOCENT CHARLES UDO <innocentcharlesudo@gmail.com>
2023-07-16 09:59:51 +0100

    Update user.py

--- 0x01-challenge/user.py
+++ 0x01-challenge/user.py
@@ -3,28 +3,23 @@
 User class
 """
 
-class User():
-    """ Documentation """
-
+class User:
     def __init__(self):
-        """ Documentation """
         self.__email = None
-
-    @email.setter
-    def email(self, value):
-        """ Documentation """
-        if type(value) is not str:
-            raise TypeError("email must be a string")
-        self.__email = value
 
     @property
     def email(self):
-        """ Documentation """
+        """Getter for email"""
         return self.__email
-   
-    
-if __name__ == "__main__":
 
+    @email.setter
+    def email(self, value):
+        """Setter for email"""
+        if not isinstance(value, str):
+            raise TypeError("email must be a string")
+        self.__email = value
+
+if __name__ == "__main__":
     u = User()
     u.email = "john@snow.com"
     print(u.email)
